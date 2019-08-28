58c910707022124bce1bc680ce642e1647926d6b
Dawid Mucha <dawidmucha98@gmail.com>
2019-08-28 18:46:02 +0200

    jumbotron under construction

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import posed from 'react-pose'
+
+class App extends React.Component {
+	render() {
+		return (
+			<div id='AppContainer'>
+				<h1>EXPENSIVA</h1>
+				<button>log in with google</button>
+				<button>register</button>
+				<button>log in</button>
+			</div>
+		)
+	}
+}
+
+export default App
